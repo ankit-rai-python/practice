465b4f152ce45ceaa4a5abf9b8d52f8e09b860c7
Ankit Rai <133601149+ankitrai1985@users.noreply.github.com>
2024-12-27 16:31:55 +0000

    Update prime_number.py

--- basics/prime_number.py
+++ basics/prime_number.py
@@ -12,4 +12,16 @@ def allPrimesUpTo(num):
             prime.append(number)
     return prime
 
+def allPrimesUpTo_more_efficient(num):
+    prime = []
+    # Your code goes here.
+    for number in range(2, num):
+        for factor in prime:
+            if number % factor ==0:
+                break
+        else:
+            prime.append(number)
+    return prime
 print("Prime number betwween 2 and 100 are: ", allPrimesUpTo(100))
+
+print("Prime number betwween 2 and 100 are: ", allPrimesUpTo_more_efficient(100))
